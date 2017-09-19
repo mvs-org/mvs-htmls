54047171822d133700e35b2203224ccdc7ac7822
Laurent Salou <laurentsalou@example.com>
2017-09-19 18:35:22 +0800

    Register Check that the Private key doesn't have special characters

--- register/register.controller.js
+++ register/register.controller.js
@@ -5,9 +5,9 @@
   .module('app')
   .controller('RegisterController', RegisterController);
 
-  RegisterController.$inject = ['MetaverseService','$scope', '$location', '$rootScope', 'FlashService', '$translate', '$window'];
+  RegisterController.$inject = ['MetaverseService','$scope', '$location', 'localStorageService', '$rootScope', 'FlashService', '$translate', '$window'];
 
-  function RegisterController(MetaverseService, $scope, $location, $rootScope, FlashService, $translate, $window) {
+  function RegisterController(MetaverseService, $scope, $location, localStorageService, $rootScope, FlashService, $translate, $window) {
     var vm = this;
 
     vm.register = register;
@@ -37,32 +37,47 @@
         $window.scrollTo(0,0);
         return;
       }
-      else if((vm.user.password_repeat!=vm.user.password) && !$scope.import_from_file){
+      else if((vm.user.password_repeat!=vm.user.password) && !$scope.import_from_file && !$scope.import_from_phrase){
         $translate('MESSAGES.PASSWORD_NOT_MATCH').then( (data) => FlashService.Error(data) );
         $window.scrollTo(0,0);
         return;
       }
       if($scope.import_from_phrase){ //Import account from phrase
-        MetaverseService.ImportAccount(vm.user.username, vm.user.password, $scope.import_phrase, $scope.address_count)
-        .then(function (response) {
-          if ( typeof response.success !== 'undefined' && response.success) {
-              $translate('MESSAGES.IMPORT_SUCCESS').then( (data) => {
-                  FlashService.Success(data,true);
-                  $location.path('/login');
+        //Remove the Enter key from the phrase
+        var re = /(\r\n|\n|\r)/gm;
+        var phraseToSend = $scope.import_phrase.replace(re," ");
+        phraseToSend = phraseToSend.replace("  "," ");
+
+        //Check if the key contains special characters
+        var occurences = phraseToSend.match(/[a-z]|[A-Z]| /g);
+        if(phraseToSend.length != occurences.length){
+          $translate('MESSAGE.WRONG_PRIVATE_KEY').then( (data) => FlashService.Error(data) );
+          $window.scrollTo(0,0);
+          return;
+        } else {
+          MetaverseService.ImportAccount(vm.user.username, vm.user.password, phraseToSend, $scope.address_count)
+          .then(function (response) {
+            if (response == undefined && typeof response.success !== 'undefined' && response.success) {
+                $translate('MESSAGES.IMPORT_SUCCESS').then( (data) => {
+                    FlashService.Success(data,true);
+                    $location.path('/login');
+                });
+            } else {
+              $translate('MESSAGES.IMPORT_ERROR').then( (data) => {
+                if (response.message != undefined) {
+                  FlashService.Error(data + " " + response.message);
+                  $window.scrollTo(0,0);
+                } else {
+                  FlashService.Error(data);
+                  $window.scrollTo(0,0);
+                }
               });
-          } else {
-            $translate('MESSAGES.IMPORT_ERROR').then( (data) => {
-              if (response.message != undefined) {
-                FlashService.Error(data + " " + response.message);
-                $window.scrollTo(0,0);
-              } else {
-                FlashService.Error(data);
-                $window.scrollTo(0,0);
-              }
-            });
-          }
-        });
+            }
+          });
+        }
       } else if($scope.import_from_file){ //Import account from file
+        //MetaverseService.ImportAccountFromFile($scope.accountInfo, vm.user.password)
+        //.then(function (response) {
         MetaverseService.ImportAccountFromFile($scope.path, vm.user.password)
         .then(function (response) {
           if ( typeof response.success !== 'undefined' && response.success) {
