f9acc9bee0dbfedd890d99a96a47d274be93d8e5
Laurent Salou <laurent.salou@hotmail.fr>
2019-01-28 16:30:12 +0800

    Server url handle set default port when unknow

--- app-services/metaverse.service.js
+++ app-services/metaverse.service.js
@@ -15,8 +15,8 @@
         var service = {};
 
         //Test runned on port 3000. Mainnet is on port 8820
-        var SERVER = window.location.hostname+((window.location.port!=80)?":"+((window.location.port==3000)?8820:window.location.port) : "");
-        var SERVER2 = window.location.hostname+((window.location.port!=80)?":"+((window.location.port==3000)?8821:parseInt(window.location.port)+1) : "");
+        var SERVER = window.location.hostname+((window.location.port!=80)?":"+((window.location.port==3000 || !window.location.port)?8820:window.location.port) : "");
+        var SERVER2 = window.location.hostname+((window.location.port!=80)?":"+((window.location.port==3000 || !window.location.port)?8821:parseInt(window.location.port)+1) : "");
 
         //var RPC_URL = window.location.protocol + '://' + SERVER + '/rpc';
         var RPC_URL = window.location.protocol + '/rpc';
